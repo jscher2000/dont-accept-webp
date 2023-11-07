25eea9332a3819f5d0c9c224b7178bd3db830694
Jefferson Scher <jscher2000@gmail.com>
2023-11-07 08:13:02 -0800

    version 0.9
    In response to Issue #10, adds automatic deconfliction for users of the "Load Reddit Images Directly" extension by making no changes to the hosts for which that extension modifies the Accept header.

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
 /* 
-  Copyright 2022. Jefferson "jscher2000" Scher. License: MPL-2.0.
+  Copyright 2023. Jefferson "jscher2000" Scher. License: MPL-2.0.
   v0.5 - initial design
   v0.5.1 - updated icons
   v0.6 - pesky commas...
   v0.7 - new menu; option to also not accept AVIF
   v0.8 - ability to exempt a site and embedded/linked files
   v0.8.1, v0.8.2 - bug fix
+  v0.9 - automatic deconfliction
 */
 
 let nowlistening; // used for toggling listener
@@ -37,12 +38,40 @@ browser.storage.local.get("prefs").then( (results) => {
 	if (oPrefs.toBlock.length > 0) startListening();
 }).catch((err) => {console.log('Error retrieving "prefs" from storage: '+err.message);});
 
+// Build deconfliction list and set up related extension management events (version 0.9)
+deconExtList = [
+	{
+		id: "{4c421bb7-c1de-4dc6-80c7-ce8625e34d24}",
+		name: "Load Reddit Images Directly",
+		hosts: ["i.redd.it", "external-preview.redd.it", "preview.redd.it"]
+	}
+];
+deconActive = [];
+function deconChecker(extInfo){
+	var dPat = deconExtList.find(objExt => objExt.id === extInfo.id);
+	if (dPat == undefined) return;
+	var indx = deconActive.indexOf(dPat.id);
+	if (extInfo.enabled == true && indx > -1) return; // no action needed
+	if (extInfo.enabled != true && indx <= -1) return; // no action needed
+	if (extInfo.enabled == true && indx <= -1) deconActive.push(dPat.id); // add to active list
+	if (extInfo.enabled != true && indx > -1) deconActive.splice(indx, 1); // remove from active list
+}
+browser.management.getAll().then((arrExtInfo) => {
+	for (var j=0; j<arrExtInfo.length; j++){
+		deconChecker(arrExtInfo[j]);
+	}
+});
+browser.management.onInstalled.addListener(deconChecker);
+browser.management.onEnabled.addListener(deconChecker);
+browser.management.onDisabled.addListener(deconChecker);
+browser.management.onUninstalled.addListener(deconChecker);
+
 /**** Clean Accept Headers of Intercepted Requests ****/
 
 function cleanAccept(details) { 
 	// v0.8 exit for hostnames on the exemptSites list [FUTURE VERSION: allow partial matches?]
+	var reqUrl = new URL(details.url);
 	if (oPrefs.exemptSites.length > 0){
-		var reqUrl = new URL(details.url);
 		if (oPrefs.exemptSites.includes(reqUrl.hostname)) return { requestHeaders: details.requestHeaders };
 		if (details.documentUrl != null && details.documentUrl != undefined){
 			var docUrl = new URL(details.documentUrl);
@@ -53,6 +82,17 @@ function cleanAccept(details) {
 			if (oPrefs.exemptSites.includes(origUrl.hostname)) return { requestHeaders: details.requestHeaders };
 		}
 	}
+
+	// v0.9 exit for hostnames on the deconfliction list
+	for (var i=0; i<deconActive.length; i++){
+		var deconObj = deconExtList.find(objExt => objExt.id === deconActive[i]);
+		var deconHosts = deconObj.hosts;
+		if (deconHosts.includes(reqUrl.hostname)){
+			console.log('Deconfliction in effect for ' + reqUrl.hostname + '; SKIPPING!');
+			return { requestHeaders: details.requestHeaders };
+		}
+	}
+
 	// examine and modify the Accept header if present
 	for (let header of details.requestHeaders) {
 		if (header.name.toLowerCase() === 'accept'){
